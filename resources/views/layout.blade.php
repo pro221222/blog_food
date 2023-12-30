32a88f21939beae88947eb922dac788d8af7c069
nguyen22-es <112994817+nguyen22-es@users.noreply.github.com>
2023-12-30 20:33:38 +0700

    like

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -189,8 +189,10 @@
         <!-- Scroll-to-top button end -->
 
         <!-- Javascript -->
+        <script src="{{ asset('js\signalr\dist\browser\signalr.min.js') }}"></script>
         <script src="{{ asset('js\main.js') }}"></script>
-
+        <script src="{{ asset('js\knockout\knockout-latest.min.js') }}"></script>
+       
         <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
         <script>
             AOS.init({
